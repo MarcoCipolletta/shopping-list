61309251661825ec11bbcdaa76964d324cf543a1
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-05-18 20:21:52 +0200

    fixed margin bottom

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  @ViewChild('footer', { static: true }) footer!: ElementRef<HTMLElement>;
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.updateFooterHeight();
+  }
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.updateFooterHeight();
+  }
+
+  private updateFooterHeight() {
+    const height = this.footer.nativeElement.offsetHeight;
+    console.log(height);
+    document.documentElement.style.setProperty(
+      '--footer-height',
+      `${height + 18}px`
+    );
+  }
 }
